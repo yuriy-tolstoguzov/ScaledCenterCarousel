3251975f24e4741e8bd7811d5b6b35efef2d7fc5
Yuriy Tolstoguzov <Yuriy.Tolstoguzov@gmail.com>
2017-04-23 21:58:16 +0300

    Added Carthage support

--- ScaledCenterCarousel/Classes/YTScaledCenterCarouselLayout.h
+++ ScaledCenterCarousel/Classes/YTScaledCenterCarouselLayout.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 yuriy-tolstoguzov. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 
 @interface YTScaledCenterCarouselLayout : UICollectionViewLayout
---
+++ ScaledCenterCarousel/ScaledCenterCarousel.h
@@ -0,0 +1,27 @@
+//
+//  ScaledCenterCarousel.h
+//  ScaledCenterCarousel
+//
+//  Created by Yuriy Tolstoguzov on 4/23/17.
+//  Copyright © 2017 Yuriy Tolstoguzov. All rights reserved.
+//
+
+#ifndef ScaledCenterCarousel_h
+#define ScaledCenterCarousel_h
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "YTScaledCenterCarouselLayout.h"
+#import "YTScaledCenterCarouselPaginator.h"
+
+#endif /* ScaledCenterCarousel_h */
